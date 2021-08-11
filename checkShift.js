bda32141a5fcac85307128641c0affcad373e795
三浦敬之 <miuratakayuki@miuratakayukinoMac-mini.local>
2021-08-12 07:25:01 +0900

    【ブランチ移設】新規

---
+++ checkShift.js
@@ -0,0 +1,243 @@
+//===============================
+//■実施想定
+//===============================
+//＜夜勤者がボタンを押したタイミングが日が変わる前の場合＞
+//当日のシフトデータと、翌日のシフトデータを取得
+
+//もしもボタンをおしたタイミングが21時から24時の場合は、「当日のシフト」と「翌日のシフト」を取得して貼り付け
+
+//＜夜勤者がボタンを押したタイミングが日が変わった後の場合＞
+//前日のシフトデータと、当日のシフトデータを取得
+
+//もしもボタンをおしたタイミングが0時から7時の場合は、「前日のシフト」と「当日のシフト」を取得して貼り付け
+
+
+//===============================
+//■ログ
+//===============================
+
+//2021.01.01
+//業務管理シートのシート名が「yyｙｙｍｍｄｄ」から「mmdd」に変更されていたため、修正
+
+
+
+//===============================
+//■確認箇所
+//===============================
+//もしも勤務者全員が取得できていなかった場合は、ここを確認。
+
+//デジステ人数が変わったら変更される。（A3セルに記入されている数字を取得している）
+function getMemberCount() {
+    var ss = SpreadsheetApp.getActiveSpreadsheet();
+    var MemberCount = ss.getSheetByName(SheetName).getRange("A3").getValue();
+    MemberCount = MemberCount - 6; //業務管理シートの木下さんまでの行数をひく
+    return MemberCount;
+}
+
+//出力先シート
+const SheetName = 'シフトデータ確認表';
+
+//業務管理シートのID
+const ManagementSheetID = '1DPQVKf7NFl2qq7xMoHlKzS1_zpVltJrhuFj4InU2xvk';
+
+
+//===============================
+//実行関数
+//===============================
+
+//実行時間で処理を分岐
+function setTwoValues() {
+    var Hour = getTime();
+    if (Hour >= 21 && Hour <= 23) {
+        var todaysShiftValues = getTodaysShift();
+        var tomorrowShiftValues = getTomorrowShift();
+        setValues(todaysShiftValues, tomorrowShiftValues);
+
+        var todaysDate = getTodaysDate();
+        var tomorrowDate = getTomorrowDate();
+        setDateValue(todaysDate, tomorrowDate);
+    }
+    if (Hour >= 0 && Hour <= 6) {
+        var lastdaysShiftValues = getLastDaysShift();
+        var todaysShiftValues = getTodaysShift();
+        setValues(lastdaysShiftValues, todaysShiftValues);
+
+        var lastDaysDate = getLastDaysDate();
+        var todaysDate = getTodaysDate();
+        setDateValue(lastDaysDate, todaysDate);
+    }
+}
+//===============================
+
+
+//現在時刻を取得（戻り値は現在の「時」）
+function getTime() {
+    var now = new Date();
+    var Hour = now.getHours();
+    return Hour;
+}
+
+//業務管理シートのシフトをイベントシートに貼り付け
+function setValues(beforeValues, afterValues) {
+    var ss = SpreadsheetApp.getActiveSpreadsheet();
+    var sheet = ss.getSheetByName(SheetName);
+
+    sheet.getRange(2, 3, getMemberCount(), 2).setValues(beforeValues);
+    sheet.getRange(2, 6, getMemberCount(), 2).setValues(afterValues);
+}
+
+function setDateValue(beforeDate, afterDate) {
+    var ss = SpreadsheetApp.getActiveSpreadsheet();
+    var sheet = ss.getSheetByName(SheetName);
+
+    sheet.getRange(1, 4, 1).setValue(beforeDate);
+    sheet.getRange(1, 7, 1).setValue(afterDate);
+}
+
+//===============================
+//前日
+//===============================
+
+//前日の年月日を取得
+function makeLastDaySheetName() {
+    var date = new Date();
+    var year = date.getFullYear();
+    var month = date.getMonth() + 1;
+    var day = date.getDate();
+
+    //もしも元日だった場合、前年大晦日にする
+    if (day === 1 && month === 1) {
+        year = year - 1;
+        month = 12;
+        day = 31;
+    }
+
+    //もしも月初一日だった場合、前月にして最終日を取得する
+    else if (day === 1) {
+        month = month - 1;
+        var dt = new Date(year, month, 0);
+        day = dt.getDate();
+    }
+
+    //上記２パターン以外だった場合は、前日を取得するためにマイナス１する
+    else {
+        day = day - 1;
+    }
+
+    month = ("0" + month).slice(-2);
+    day = ("0" + day).slice(-2);
+    var lastDaySheetName = month + day;
+    return lastDaySheetName;//mmdd
+}
+
+//前日の業務管理シートを取得
+function getLastDaysShift() {
+    var ss = SpreadsheetApp.openById(ManagementSheetID);
+    var lastDaySheetName = makeLastDaySheetName();
+    var lastDaySheet = ss.getSheetByName(lastDaySheetName);
+    var lastdaysShiftArea = lastDaySheet.getRange(7, 1, getMemberCount(), 2);
+    var lastdaysShiftValues = lastdaysShiftArea.getValues();
+    return lastdaysShiftValues;
+}
+
+//前日の業務管理シートの曜日を取得
+function getLastDaysDate() {
+    var ss = SpreadsheetApp.openById(ManagementSheetID);
+    var lastDaySheetName = makeLastDaySheetName();
+    var lastDaySheet = ss.getSheetByName(lastDaySheetName);
+    var lastDaysDate = lastDaySheet.getRange("AW4").getValue();
+    return lastDaysDate;
+}
+
+
+//===============================
+//当日
+//===============================
+
+//当日の月日を取得
+function makeTodaySheetName() {
+    var date = new Date();
+    var month = date.getMonth() + 1;
+    var day = date.getDate();
+
+    month = ("0" + month).slice(-2);
+    day = ("0" + day).slice(-2);
+    var todaySheetName = month + day;
+    return todaySheetName;//mmdd
+}
+
+//当日の業務管理シートを取得
+function getTodaysShift() {
+    var ss = SpreadsheetApp.openById(ManagementSheetID);
+    var todaySheetName = makeTodaySheetName();
+    var todaySheet = ss.getSheetByName(todaySheetName);
+    var todaysShiftArea = todaySheet.getRange(7, 1, getMemberCount(), 2);
+    var todaysShiftValues = todaysShiftArea.getValues();
+    return todaysShiftValues;
+}
+
+//当日の業務管理シートを取得
+function getTodaysDate() {
+    var ss = SpreadsheetApp.openById(ManagementSheetID);
+    var todaySheetName = makeTodaySheetName();
+    var todaySheet = ss.getSheetByName(todaySheetName);
+    var todaysDate = todaySheet.getRange("AW4").getValue();
+    return todaysDate;
+}
+
+
+//===============================
+//　翌日
+//===============================
+
+//翌日の年月日を取得
+function makeTomorrowSheetName() {
+    var date = new Date();
+    var year = date.getFullYear();
+    var month = date.getMonth() + 1;
+    var day = date.getDate();
+
+    var dt = new Date(year, month, 0);
+    var lastDay = dt.getDate();
+
+    //もしも大晦日にボタンを押した場合、年を１プラスする
+    if (day === 31 && month === 12) {
+        year = year + 1;
+        month = 1;
+        day = 1;
+    }
+
+    //もしも月末にボタンを押した場合、翌月にして日を月初にする
+    if (lastDay === day) {
+        month = month + 1;
+        day = 1;
+    }
+
+    //月末以外だった場合は、取得した日数に１プラスする
+    if (day !== lastDay) {
+        day = day + 1;
+    }
+
+    month = ("0" + month).slice(-2);
+    day = ("0" + day).slice(-2);
+    var tomorrowSheetName = month + day;//ｍｍｄｄ
+    return tomorrowSheetName;
+}
+
+//翌日の業務管理シートを取得
+function getTomorrowShift() {
+    var ss = SpreadsheetApp.openById(ManagementSheetID);
+    var tomorrowSheetName = makeTomorrowSheetName();
+    var tomorrowSheet = ss.getSheetByName(tomorrowSheetName);
+    var tomorrowShiftArea = tomorrowSheet.getRange(7, 1, getMemberCount(), 2);
+    var tomorrowShiftValues = tomorrowShiftArea.getValues();
+    return tomorrowShiftValues;
+}
+
+function getTomorrowDate() {
+    var ss = SpreadsheetApp.openById(ManagementSheetID);
+    var tomorrowSheetName = makeTomorrowSheetName();
+    var tomorrowSheet = ss.getSheetByName(tomorrowSheetName);
+    var tommorrowDate = tomorrowSheet.getRange("AW4").getValues();
+    return tommorrowDate;
+}
---
+++ sortValues.js
@@ -0,0 +1,223 @@
+//===============================
+//実行関数
+//===============================
+function action() {
+  //マスタシートからタスクと出勤時間を取得
+  const masterData = getMasterData();
+
+  //マスタシートのデータを出勤時間の早い順にソート
+  sortMasterData(masterData);
+
+  //マスタのデータの最終行を取得
+  const row_last_num = getMasterColumn();
+
+  //当日のシフトデータを取得（休以外）
+  //const Shift = getNextShiftData();
+
+  //シフトを取得してな書き込み
+  getShiftToDrew(row_last_num);
+}
+
+//===============================
+//業務管理シートを取得
+//===============================
+function getManagementSheet() {
+  var managementSS = SpreadsheetApp.openById(ManagementSheetID);
+  return managementSS;
+}
+
+//===============================
+//F列G列：2行目-最終行を取得して、休みを除外した配列を作成する（未使用）
+//===============================
+
+// function getNextShiftData() {
+//     var SS = SpreadsheetApp.getActiveSpreadsheet();
+//     var sheet = SS.getActiveSheet();
+//     var nextShiftData = sheet.getRange(2, 6, 32, 2).getValues();
+
+//     //    Logger.log(nextShiftData);
+//     //    Logger.log(nextShiftData[0]);//[木下,休]
+//     //    Logger.log(nextShiftData[0][0]);//木下
+//     //    Logger.log(nextShiftData[0][1]);//休
+//     //    Logger.log(nextShiftData.length);//32
+
+//     var Shift = [];
+//     for (var i = 0; i < nextShiftData.length; i++) {
+//         if (nextShiftData[i][1] === "休") {
+//             continue;
+//         }
+//         //「休」以外のシフトを配列Shiftに追加
+//         Shift.push(nextShiftData[i]);
+//     }
+//     Logger.log(Shift)
+//     return Shift;
+// }
+
+//===============================
+//C2からP40の値を削除
+//===============================
+function deleteValues() {
+  var SS = SpreadsheetApp.getActiveSpreadsheet();
+  var sheet = SS.getActiveSheet();
+  var range = sheet.getRange("C2:P40");
+  range.clearContent();
+}
+
+//===============================
+//業務管理シートのマスタデータを取得
+//===============================
+function getMasterData() {
+  var SS = SpreadsheetApp.getActiveSpreadsheet();
+  var sheet = SS.getActiveSheet();
+  var nextShiftDate = sheet.getRange("G1").getValues();
+  // var nextShiftDate = "祝日";
+  var ManagementSS = getManagementSheet();
+
+  if (nextShiftDate == "平日") {
+    var lastRow = ManagementSS.getSheetByName("平日マスタ").getLastRow();
+    var masterDataRange = ManagementSS.getSheetByName("平日マスタ").getRange(
+      2,
+      1,
+      lastRow - 3,
+      2
+    ); //0:00の行を除外するため-3
+    var masterData = masterDataRange.getDisplayValues();
+    return masterData;
+  }
+  if (nextShiftDate == "土日") {
+    var lastRow = ManagementSS.getSheetByName("土日マスタ").getLastRow();
+    var masterDataRange = ManagementSS.getSheetByName("土日マスタ").getRange(
+      2,
+      1,
+      lastRow - 3,
+      2
+    );
+    var masterData = masterDataRange.getDisplayValues();
+    Logger.log(masterData);
+    return masterData;
+  }
+  if (nextShiftDate == "祝日") {
+    var lastRow = ManagementSS.getSheetByName("祝日マスタ").getLastRow();
+    var masterDataRange = ManagementSS.getSheetByName("祝日マスタ").getRange(
+      2,
+      1,
+      lastRow - 3,
+      2
+    );
+    var masterData = masterDataRange.getDisplayValues();
+    return masterData;
+  }
+}
+
+//===============================
+//業務管理シートのマスタデータを貼り付け　→　時間の早い順にソート
+//===============================
+//貼り付ける前に削除する必要がある
+
+function sortMasterData(masterData) {
+  const masterDataCount = masterData.length;
+  var SS = SpreadsheetApp.getActiveSpreadsheet();
+  var range = SS.getActiveSheet().getRange(2, 10, masterDataCount, 2);
+  range.setValues(masterData);
+  range.sort([{ column: 11, ascending: true }]);
+}
+
+//===============================
+//貼り付けたマスターデータのタスクが記載されている列を取得する →　最終行を取得
+//===============================
+
+function getMasterColumn() {
+  var SS = SpreadsheetApp.getActiveSpreadsheet();
+  var sheet = SS.getActiveSheet();
+  var ss_range = sheet.getRange("J:J").getValues();
+  var row_last_num = ss_range.filter(String).length;
+  return row_last_num;
+}
+
+//===============================
+//J列の値を上からG列内で検索し、ヒットしたらその行のF列の名前を取得
+//取得した名前を検索したJ列の行のL列に記述
+//休みなら飛ばす
+//G列の値の数だけ実施する
+//===============================
+
+function getShiftToDrew(row_last_num) {
+  var SS = SpreadsheetApp.getActiveSpreadsheet();
+  var sheet = SS.getActiveSheet();
+
+  //メンバーの数だけ実施
+  var memberCount = getMemberCount();
+  var row = 1;
+  while (row <= memberCount) {
+    //1<=32true
+
+    //G列2行目をとってきてsfにいれる  1+1
+    var sf = sheet.getRange(1 + row, 7, 1, 1);
+
+    //sfの値をShiftにいれる
+    var Shift = sf.getValue();
+
+    if (Shift === "") {
+      break;
+    }
+
+    //値が「休」だったら、とばす（次の行にいくために+1）
+    if (Shift === "休") {
+      row = row + 1;
+      continue;
+    }
+
+    // 初期値を2で行場号の箱rowNumに入れる(初期化)
+    var rowNum = 1;
+
+    // Shiftのデータが空じゃなかったら処理をする
+    //さらに「休」じゃなかったら、シートのJ列を取得する
+    while (rowNum <= row_last_num) {
+      var sh = sheet.getRange(1 + rowNum, 10, 1, 1); //J列
+
+      //J列の値をMasterにいれる
+      var Master = sh.getValue();
+
+      //もしもShiftとMasterが違ったら次のJ列にいくため１足、終了
+      if (Shift !== Master) {
+        rowNum = rowNum + 1;
+        continue;
+      }
+
+      //もしもShiftの値とMasterの値が同じだったら
+      if (Shift === Master) {
+        // Shiftのセルをアクティブにして
+        sf.activate();
+
+        // 左となりの値をnameに取得する
+        var name = sf.offset(0, -1).getValue();
+
+        //Masterのセルをアクティブにしてセルチェックカウントを０に。
+        sh.activate();
+        var cellcheckcount = 0;
+        while (cellcheckcount < 5) {
+          //5人までできる。それ以上はこの数字を変える。５なら6人目以降はスルーされる。
+          var checkedCell = sh.offset(0, 2 + cellcheckcount);
+          var checkedValue = checkedCell.getValue();
+          if (checkedValue !== "") {
+            cellcheckcount++;
+            continue;
+          }
+
+          if (checkedValue === "") {
+            checkedCell.setValue(name);
+            break;
+          }
+        }
+
+        // sh.offset(0, 2).setValue(name);
+        //Masterのセルの右に２つずれたところにセットする
+
+        //セットが終わったら、次の行をチェックするために１足す
+        rowNum = rowNum + 1;
+        continue;
+      }
+    }
+    row = row + 1;
+  }
+}
